08b8c88c46c6cb4c3cf5deea1cd11dfdc35831f6
J-E-J-S <james.sanders1711@gmail.com>
2022-01-08 09:44:10 +0000

    added snapgene only open

--- src/snapgene.sh
+++ src/snapgene.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 # opens a file with snapgene
 
+# open snapgene without file
+if [ $# -eq 0 ]; then
+    {
+        start "" 'C:\Program Files\snapgene\SnapGene.exe'
+        exit 0
+    } || {
+        echo "Snapgene not found, check path."
+        exit 1
+    }
+fi
+
+# check file exists
 if ! [[ -f $1 ]] ; then
     echo 'File does not exist.'
     exit 1
 fi
 
+# check file is compatible with snapgene
 if [[ $1 != *.fasta ]] && [[ $1 != *.fas ]] && [[ $1 != *.fa ]] &&  [[ $1 != *.gb ]] && [[ $1 != *.dna ]]; then
     echo "File format is not compatible."
     echo "Compatible formats: .fasta, .fas, .fa, .gb, .dna "
     exit 1
 fi
 
+# handle bad snapgene path error
 {
     start "" 'C:\Program Files\snapgene\SnapGene.exe' $1
+    exit 0
 } || {
     echo "Snapgene not found, check path."
+    exit 1
 }
-exit 0
